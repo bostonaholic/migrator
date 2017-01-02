2c66d5a95fad70d2193e7f4c52be6975d5887ae3
Matthew Boston <matthew@matthewboston.com>
2017-01-02 09:10:37 -0700

    update project metadata

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject bostonaholic/migrator "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A Clojure library for database migrations."
+  :url "https://github.com/bostonaholic/migrator"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]])
