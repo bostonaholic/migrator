eb796fbeb595760dc5f1b001636eefb9aaef14ff
Matthew Boston <matthew@matthewboston.com>
2017-01-02 09:05:22 -0700

    initial commit

---
+++ src/bostonaholic/migrator.clj
@@ -0,0 +1,6 @@
+(ns bostonaholic.migrator)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/bostonaholic/migrator_test.clj
@@ -0,0 +1,7 @@
+(ns bostonaholic.migrator-test
+  (:require [clojure.test :refer :all]
+            [bostonaholic.migrator :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
